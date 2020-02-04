902242a007318897c467f220b0ddba0631787b19
Abadi <h.abadi@student.tue.nl>
2020-02-04 12:18:56 +0100

    Instructon1

---
+++ Instruction1/example2b/mylib.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int read ( void );
+
+void split
+
+	( int     amount ,
+	  int*    count  ,
+	  int*    coins  );
+
+
+void report
+
+	( int     amount ,
+	  int*    count  ,
+	  int*    coins  ,
+	  int     n      );
+
+void check
+    ( int     amount 
+	  );
---
+++ Instruction1/example2c/mylib.h
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+	int      count;
+	int      value;
+	char     *name;
+} Coin;
+
+int init
+
+	( Coin* coins );
+
+void reset
+	
+	( Coin* coins ,
+	  int	 n     );
+
+
+int read ( void );
+
+void split
+
+	( int     amount ,
+	  Coin*   coins  );
+
+
+void report
+
+	( int     amount ,
+	  Coin*   coins  ,
+	  int     n      );
+
+void check
+    (int amount);
